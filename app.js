7cfa33149d1f1e9a417e6db2d454d9575abdbe1c
Akshita44 <akshitasharma407@gmail.com>
2021-11-07 00:09:23 +0530

    deleted default home path

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ const cookieparser=require("cookie-parser")
 require("./db/conn");
 app.use(express.json())
 app.use(cookieparser())
-app.use("/images",express.static(__dirname,"/images"))
+app.use("/images",function(req, res, next) {
+    req.getUrl = function() {
+      return req.protocol + "://" + req.get('host') +"/images";
+    }
+    return next();
+  });
+// app.use("/images",express.static(__dirname,"/images"))
 // app.use(express.urlencoded({extended:false}));
 const userrouter=require("./router/auth");
 const postrouter=require("./router/posts");
