8e82671636dec13b58cecdc6c9265f7ccb4a5b64
Akshita44 <akshitasharma407@gmail.com>
2022-12-27 14:54:41 +0530

    updated application

--- app.js
+++ app.js
@@ -22,11 +22,16 @@ app.use("/post",postrouter)
 app.use("/categories",categoryrouter)
 app.use("/comment",commentsrouter)
 
-if(process.env.NODE_ENV === "production")
+if(process.env.NODE_ENV === "production" || process.env.NODE_ENV === "prod")
 {
-    app.use(express.static(path.resolve(__dirname,"./client","build")));
+    app.use(express.static(path.join(__dirname,"./client/build")));
     app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"./client","build","index.html"))
+        res.sendFile(path.join(__dirname,"./client/build/index.html"),
+        function(err)
+        {
+            res.status(500).send(err);
+        }
+        )
     })
 }
 app.listen(port,()=>{
